8826963bb902f070791c432765ac9625d91d938f
Damien Sedgwick <damien@adaptdigital.co>
2023-06-28 09:37:03 +0100

    setup email notifications

--- apps/payments/src/payments.service.ts
+++ apps/payments/src/payments.service.ts
@@ -1,22 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import Stripe from 'stripe';
 import { ConfigService } from '@nestjs/config';
-import { CreateChargeDto } from '@app/common/dto/create-charge.dto';
+import { NOTIFICATIONS_SERVICE } from '@app/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { PaymentsCreateChargeDto } from './dto/payments-create-charge.dto';
 
 @Injectable()
 export class PaymentsService {
-  constructor(private readonly config: ConfigService) {}
+  constructor(
+    private readonly config: ConfigService,
+    @Inject(NOTIFICATIONS_SERVICE)
+    private readonly notificationsService: ClientProxy,
+  ) {}
 
   private readonly stripe = new Stripe(this.config.get('STRIPE_SECRET_KEY'), {
     apiVersion: '2022-11-15',
   });
 
-  async createCharge({ amount }: CreateChargeDto) {
-    return await this.stripe.paymentIntents.create({
+  async createCharge({ amount, email }: PaymentsCreateChargeDto) {
+    const intent = await this.stripe.paymentIntents.create({
       amount: amount * 100,
       currency: 'gbp',
       confirm: true,
       payment_method: 'pm_card_visa',
     });
+
+    this.notificationsService.emit('notify_email', { email: email });
+
+    return intent;
   }
 }
